7dd649ed18bddfc9263e7410cb18fe3bfecd6bfc
Igors78 <oleinikovsigors@gmail.com>
2020-10-27 15:10:54 +0200

    my_each method defined

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,21 @@
-module Enumerable
-    def my_each
-        for x in arr do
-            yield
-          end
+class Array
+  def my_each
+    for item in self
+      yield(item)
+    end
+  end
+end
+
+arr = [1, 2, 3]
+arr.my_each do
+  |x| 
+  puts x + 10
+  end
+
+  class Array
+    def my_each_with_index
+      for item in self
+        yield(item)
+      end
     end
   end
