9197bb5948b53efd0816fd560de70be37e5fb0c9
Lameck1 <lameck72@gmail.com>
2020-10-27 16:43:48 +0300

    Add my_each_with_index and my_select

--- enumerables.rb
+++ enumerables.rb
@@ -1,21 +1,31 @@
 class Array
-  def my_each
+  def my_each 
     for item in self
       yield(item)
     end
   end
-end
 
-arr = [1, 2, 3]
-arr.my_each do
-  |x| 
-  puts x + 10
-  end
-
-  class Array
-    def my_each_with_index
-      for item in self
-        yield(item)
-      end
+  def my_each_with_index 
+    self.length.times do |index|
+      yield self[index], index
     end
   end
+
+  def my_select
+    selected = []
+    my_each do |ele|
+      selected.push(ele) if yield ele
+    end
+    p selected
+  end
+
+end
+
+
+
+arr = ["Tom", "Bob", "Marley"]
+# arr.my_each do |x|
+#   puts x + 10
+# end
+
+arr.my_select{|person| person != "Tom"}
