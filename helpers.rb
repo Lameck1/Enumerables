d68b214d358554d800e901fe49b00abf8291c7a7
Lameck1 <lameck72@gmail.com>
2020-10-28 14:44:51 +0300

    Add match_by_type_any helper

--- helpers.rb
+++ helpers.rb
@@ -16,4 +16,22 @@ module Helper
     end
     true
   end
+
+  def self.match_by_type_any(arg, type)
+    case type
+    when Regexp
+      arg.my_each do |item|
+        return true if type.match(ele.to_s)
+      end
+    when Class
+      arg.my_each do |item|
+        return true if ele.is_a?(type)
+      end
+    else
+      arg.my_each do |item|
+        return true if item == type
+      end
+    end
+    false
+  end
 end
